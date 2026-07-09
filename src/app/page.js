5d8bdb1fd38684e9678da360ce5d6ae3367257a5
Afrina Tithy <afrina.tithy@gmail.com>
2026-07-09 11:00:02 +0600

    Create sidebar component structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
-export default function home() {
+export default function Home() {
   return (
-    <h1>Smart Finance Dashboard</h1>
+    <div className="bg-red-500 text-white text-4xl font-bold h-screen flex items-center justify-center">
+      Tailwind is Working 🚀
+    </div>
   );
 }
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,67 @@
+import Link from "next/link";
+import {
+    LayoutDashboard,
+    ReceiptText,
+    ChartColumn,
+    Wallet,
+    Settings,
+} from "lucide-react";
+
+
+export default function Sidebar() {
+    return (
+        <aside className="w-64 min-h-screen bg-white border-r border-gray-200 p-6 hidden md:flex flex-col">
+            <div className="mb-10">
+                <h1 className="text-2xl font-bold text-blue-500">
+                    Finance
+                </h1>
+            </div>
+
+            <nav>
+                <ul className="space-y-3">
+                    <li>
+                        <Link href="/dashboard"
+                            className="flex items-center gap-3 rounded-lg hover:bg-gray-200">
+                            <LayoutDashboard size={20} />
+                            <span>Dashboard</span>
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link href="/transactions"
+                            className="flex items-center gap-3 rounded-lg hover:bg-gray-200">
+                            <ReceiptText size={20} />
+                            <span>Transactions</span>
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link href="/statistics"
+                            className="flex items-center gap-3 rounded-lg hover:bg-gray-200">
+                            <ChartColumn size={20} />
+                            <span>Statistics</span>
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link href="/budget"
+                            className="flex items-center gap-3 rounded-lg hover:bg-gray-200">
+                            <Wallet size={20} />
+                            <span>Budget</span>
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link href="/settings"
+                            className="flex items-center gap-3 rounded-lg hover:bg-gray-200">
+                            <Wallet size={20} />
+                            <span>Settings</span>
+                        </Link>
+                    </li>
+
+                </ul>
+            </nav>
+
+        </aside>
+    )
+}
